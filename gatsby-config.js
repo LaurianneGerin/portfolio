b0592419b67e842a06875f9c8506fdf2a7b32f6f
Laurianne Gerin <laurianne.gerin@gmail.com>
2022-09-07 08:32:45 +0100

    add prefix repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "LGerin",
   },
+  pathPrefix: "/portfolio",
   plugins: [
     "gatsby-plugin-mdx-embed",
     "gatsby-plugin-sass",
